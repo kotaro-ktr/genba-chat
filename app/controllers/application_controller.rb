1068e8b38afb78d319bccd8f39f8fa76252695f8
kotaro-ktr <83695177+kotaro-ktr@users.noreply.github.com>
2021-12-19 20:10:45 +0900

    Basic認証導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
   
 private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |user_name, password|
+      user_name == 'mkyn' && password == '9073'
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
