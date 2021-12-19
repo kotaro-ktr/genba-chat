9aa7d956a3cb0a5e96a484e3c602f2dc66b52e8d
kotaro-ktr <83695177+kotaro-ktr@users.noreply.github.com>
2021-12-19 19:58:13 +0900

    チャート機能実装５

--- app/javascript/chat.js
+++ app/javascript/chat.js
@@ -2,6 +2,7 @@ window.addEventListener('load', function(){
 
   const pullDownButton = document.getElementById("action_menu_btn")
   const pullDownParents = document.getElementById("action_menu") 
+  if (!pullDownButton){ return false;}
 
   pullDownButton.addEventListener('click', function(){
   if (pullDownParents.getAttribute("style") == "display:block;"){
